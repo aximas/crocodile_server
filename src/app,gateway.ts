8165638cabdbf50cc713615d59539904727fb3c0
aximas <northdakota778@gmail.com>
2022-08-20 23:49:35 +0500

    first message

---
+++ src/app,gateway.ts
@@ -0,0 +1,61 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+type PaintCoords = {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+};
+
+@WebSocketGateway({ cors: true })
+export class AppGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('send_message')
+  async onSendMessage(
+    @MessageBody() data: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const sockets = await this.server.fetchSockets();
+    sockets.forEach((s) => {
+      if (s.id !== socket.id) {
+        socket.broadcast.to('test').emit('receive_message', data);
+      }
+    });
+  }
+
+  @SubscribeMessage('paint')
+  async painting(
+    @MessageBody() data: PaintCoords,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const sockets = await this.server.fetchSockets();
+    sockets.forEach((s) => {
+      if (s.id !== socket.id) {
+        socket.broadcast.to('test').emit('repaint', data);
+      }
+    });
+  }
+
+  @SubscribeMessage('clear')
+  async clear(@ConnectedSocket() socket: Socket) {
+    const sockets = await this.server.fetchSockets();
+    sockets.forEach((s) => {
+      if (s.id !== socket.id) {
+        socket.broadcast.to('test').emit('clear_canvas');
+      }
+    });
+  }
+
+  handleConnection(socket: Socket) {
+    socket.join('test');
+    console.log('Connected', socket.id);
+  }
+}
